70a8f2b1e76ec0c01810bcc0c370d1aee6feba1c
Denis8871 <145900110+Denis8871@users.noreply.github.com>
2024-04-23 15:18:15 +0300

    AssemblyInfo.cs

--- Lab 15 OOP/Properties/AssemblyInfo.cs
+++ Lab 15 OOP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab 15 OOP Dikiy")]
+[assembly: AssemblyTitle("Lab 15 OOP Shostya")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab 15 OOP Dikiy")]
+[assembly: AssemblyProduct("Lab 15 OOP Shostya")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
